b43a94a7b9109853a9285dac68c5c91ec9a78615
Dimitar Danailov <dimityr.danailov@gmail.com>
2017-07-12 19:58:33 +0300

    I added materials from lecture 3.

--- CardStanford/CardGameViewController.m
+++ CardStanford/CardGameViewController.m
@@ -7,14 +7,28 @@
 //
 
 #import "CardGameViewController.h"
+#import "PlayingCardDeck.h"
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *flipsCounter;
 @property (nonatomic) int flipCount;
+@property (nonatomic, strong) Deck *deck;
 
 @end
 
 @implementation ViewController
+
+- (Deck *)deck
+{
+    if (!_deck) _deck = [self createDeck];
+    
+    return _deck;
+}
+
+- (Deck *)createDeck
+{
+    return [[PlayingCardDeck alloc] init];
+}
 
 
 - (void)viewDidLoad {
@@ -38,12 +52,18 @@
     if ([sender.currentTitle length]) {
         [sender setBackgroundImage:[UIImage imageNamed:@"cardback"] forState:UIControlStateNormal];
         [sender setTitle:@"" forState:UIControlStateNormal];
+        
+        self.flipCount++;
     } else {
-        [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"] forState:UIControlStateNormal];
-        [sender setTitle:@"A♠︎" forState:UIControlStateNormal];
+        Card *card = [self.deck drawRandomCard];
+        
+        if (card) {
+            [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"] forState:UIControlStateNormal];
+            [sender setTitle:card.contents forState:UIControlStateNormal];
+            
+            self.flipCount++;
+        }
     }
-    
-    self.flipCount++;
 }
 
 
